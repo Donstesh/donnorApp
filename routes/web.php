aafd19d152aa5623863ccd210a037f045b33ce2f
Donstesh <stephenshifoko@gmail.com>
2022-04-10 01:02:01 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,54 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Auth::routes();
+/*------------------------------------------
+
+--------------------------------------------
+
+All Normal Users Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:user'])->group(function () {
+
+  
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+});
+
+  
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Admin Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
+
+Route::middleware(['auth', 'user-access:admin'])->group(function () {
+
+  
+
+    Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home');
+
+});
+
+  
+
+/*------------------------------------------
+
+--------------------------------------------
+
+All Admin Routes List
+
+--------------------------------------------
+
+--------------------------------------------*/
